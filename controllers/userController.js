3b221a7d0485b0768a83dba646e4da232d1f6523
Taylor Shen <shen.taylor@yahoo.com>
2022-05-31 09:28:12 -0700

    updating user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -77,7 +77,7 @@ module.exports = {
         .catch((err) => res.status(500).json(err));
     },
     removeFriend (req, res) {
-        User.findOneAndUpdate({ _id: req.params.userId}, {$pull: { friend: {friendId: req.params.friendId}}}, {runValidators: true, new: true})
+        User.findOneAndUpdate({ _id: req.params.userId}, {$pull: { friends: {friendId: req.params.friendId}}}, {runValidators: true, new: true})
         .then ((user) => 
             !user
                 ? res.status(404).json({message: 'No user found with that ID'})
